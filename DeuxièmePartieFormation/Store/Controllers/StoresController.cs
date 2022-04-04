f4652337bed39bbe1e56f6bff7f3d60c46ff101d
Guiliguigui <gui.mairesse@gmail.com>
2022-04-04 14:09:06 +0200

    Store

---
+++ DeuxièmePartieFormation/Store/Controllers/StoresController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Store.Models;
+
+namespace Store.Controllers
+{
+    public class StoresController : Controller
+    {
+        private readonly masterContext _context;
+
+        public StoresController(masterContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Stores
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Stores.ToListAsync());
+        }
+
+        // GET: Stores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Stores
+                .FirstOrDefaultAsync(m => m.StoreId == id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return View(store);
+        }
+
+        // GET: Stores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Stores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StoreId,Name,Street,StreetNumber,City,Country,IsActive")] Store store)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(store);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(store);
+        }
+
+        // GET: Stores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Stores.FindAsync(id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+            return View(store);
+        }
+
+        // POST: Stores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("StoreId,Name,Street,StreetNumber,City,Country,IsActive")] Store store)
+        {
+            if (id != store.StoreId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(store);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StoreExists(store.StoreId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(store);
+        }
+
+        // GET: Stores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var store = await _context.Stores
+                .FirstOrDefaultAsync(m => m.StoreId == id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            return View(store);
+        }
+
+        // POST: Stores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var store = await _context.Stores.FindAsync(id);
+            _context.Stores.Remove(store);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StoreExists(int id)
+        {
+            return _context.Stores.Any(e => e.StoreId == id);
+        }
+    }
+}
---
+++ DeuxièmePartieFormation/Store/SQL/Tables.sql
@@ -0,0 +1,63 @@
+﻿DROP TABLE [SoldItems]
+DROP TABLE [Stores]
+
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[SoldItems](
+    [SoldItemID] [int] IDENTITY(1,1) NOT NULL,
+    [StoreID] [int] NOT NULL,
+    [Number] [int] NOT NULL,
+    [Description] [varchar](150) NOT NULL,
+    [Price] [money] NOT NULL,
+    [Quantity] [int] NOT NULL,
+ CONSTRAINT [PK_SoldItems] PRIMARY KEY CLUSTERED 
+(
+    [SoldItemID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ 
+ 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Stores](
+    [StoreID] [int] IDENTITY(1,1) NOT NULL,
+    [Name] [varchar](50) NOT NULL,
+    [Street] [varchar](50) NOT NULL,
+    [StreetNumber] [varchar](10) NOT NULL,
+    [City] [varchar](50) NOT NULL,
+    [Country] [varchar](50) NOT NULL,
+    [IsActive] [bit] NOT NULL,
+ CONSTRAINT [PK_Stores] PRIMARY KEY CLUSTERED 
+(
+    [StoreID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[SoldItems] ON
+ 
+INSERT [dbo].[SoldItems] ([SoldItemID], [StoreID], [Number], [Description], [Price], [Quantity]) VALUES (1, 1, 985649082, N'Shoes', 60.0000, 1)
+INSERT [dbo].[SoldItems] ([SoldItemID], [StoreID], [Number], [Description], [Price], [Quantity]) VALUES (2, 2, 439849329, N'T-Shirt with logo', 70.0000, 2)
+INSERT [dbo].[SoldItems] ([SoldItemID], [StoreID], [Number], [Description], [Price], [Quantity]) VALUES (3, 1, 328494300, N'Black Jeans', 100.0000, 1)
+INSERT [dbo].[SoldItems] ([SoldItemID], [StoreID], [Number], [Description], [Price], [Quantity]) VALUES (4, 1, 432874373, N'Blue Jeans', 60.0000, 3)
+SET IDENTITY_INSERT [dbo].[SoldItems] OFF
+SET IDENTITY_INSERT [dbo].[Stores] ON
+ 
+INSERT [dbo].[Stores] ([StoreID], [Name], [Street], [StreetNumber], [City], [Country], [IsActive]) VALUES (1, N'StoreOne', N'Test', N'12', N'Pie', N'United States', 1)
+INSERT [dbo].[Stores] ([StoreID], [Name], [Street], [StreetNumber], [City], [Country], [IsActive]) VALUES (2, N'Monument', N'Gray', N'454', N'Super', N'Canada', 1)
+SET IDENTITY_INSERT [dbo].[Stores] OFF
+ALTER TABLE [dbo].[SoldItems]  WITH CHECK ADD  CONSTRAINT [FK_SoldItems_Stores] FOREIGN KEY([StoreID])
+REFERENCES [dbo].[Stores] ([StoreID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[SoldItems] CHECK CONSTRAINT [FK_SoldItems_Stores]
+GO
+USE [master]
+GO
+ALTER DATABASE [Corporation] SET  READ_WRITE 
+GO
