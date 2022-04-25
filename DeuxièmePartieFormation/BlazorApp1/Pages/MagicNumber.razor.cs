8246324f59440e87496c2f72848882bb475a07b1
Guiliguigui <gui.mairesse@gmail.com>
2022-04-25 15:55:28 +0200

    Blazor MagicNumber

---
+++ DeuxièmePartieFormation/BlazorApp1/Pages/MagicNumber.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorApp1.Pages
+{
+    public class MagicNumberBase : ComponentBase
+    {
+        protected const int max = 20;
+        protected int MagicNumber { get; set; }
+        protected int Number { get; set; }
+        protected int Lifes { get; set; } = 5;
+        protected string LifesString => Lifes > 0 ? new string('❤', Lifes) : "";
+
+        [Inject]
+        protected NavigationManager _navigationManager { get; set; }
+        protected MagicNumberBase()
+        {
+            Random rand = new Random();
+            MagicNumber = rand.Next(max + 1);
+        }
+
+        protected void TestNumber()
+        {
+            if (MagicNumber == Number)
+            {
+                _navigationManager.NavigateTo("/MagicNumber/Result/true");
+                return;
+            }
+            Lifes--;
+            if (Lifes == 0)
+            {
+                _navigationManager.NavigateTo("/MagicNumber/Result/false");
+            }
+        }
+    }
+}
